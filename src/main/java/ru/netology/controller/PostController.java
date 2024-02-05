f0c7764766a5a1cafd746d70988d3acb204ed841
Kurtukov Pavel <pavel@kurtukov.ru>
2024-02-06 01:07:15 +0700

    Реализация репозитория

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -10,32 +10,37 @@ import java.io.Reader;
 
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
+  private static final String DELETE_POST_SUCCESS = "Пост id = %d удалён";
   private final PostService service;
+  private final Gson gson = new Gson();
 
   public PostController(PostService service) {
     this.service = service;
   }
 
   public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
     final var data = service.all();
-    final var gson = new Gson();
+    response.setContentType(APPLICATION_JSON);
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    final var data = service.getById(id);
+    response.setContentType(APPLICATION_JSON);
+    response.getWriter().print(gson.toJson(data));
   }
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
     final var post = gson.fromJson(body, Post.class);
     final var data = service.save(post);
+    response.setContentType(APPLICATION_JSON);
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+    final var data = String.format(DELETE_POST_SUCCESS, id);
+    service.removeById(id);
+    response.setContentType(APPLICATION_JSON);
+    response.getWriter().print(gson.toJson(data));
   }
 }
--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,48 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+  private static final String POST_NOT_FOUND = "Пост с id = %d не найден";
+  private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+  private final AtomicLong postId = new AtomicLong();
+
+  public List<Post> all() { return new ArrayList<>(posts.values()); }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(posts.get(id));
   }
 
   public Post save(Post post) {
-    return post;
+    Post savingPost;
+    long id = post.getId();
+    if (post.getId() == 0) {
+      postId.incrementAndGet();
+      savingPost = new Post(postId.get(), post.getContent());
+      posts.put(postId.get(), savingPost);
+    } else if (posts.containsKey(id)) {
+      savingPost = new Post(id, post.getContent());
+      posts.replace(id, post);
+    } else {
+      throw new NotFoundException(String.format(POST_NOT_FOUND, id));
+    }
+    return savingPost;
   }
 
   public void removeById(long id) {
+    if (posts.containsKey(id)) {
+      posts.remove(id);
+    } else {
+      throw new NotFoundException(String.format(POST_NOT_FOUND, id));
+    }
   }
 }
